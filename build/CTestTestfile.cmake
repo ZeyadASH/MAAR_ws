e19461a24bedea697971756f8475bd241d71e9cd
ZEY <zeyadashraf10111@gmail.com>
2023-04-29 15:53:08 +0300

    Initial Commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/zey/MAAR_ws/src
+# Build directory: /home/zey/MAAR_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
